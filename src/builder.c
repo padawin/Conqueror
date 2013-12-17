6ef01fe76196d33022cb6c26345e378e7d44d4b7
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-12-17 16:37:25 +0000

    builder created to have predefined boards

---
+++ src/builder.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "builder.h"
+#include "entity/board.h"
+
+/**
+ * A small board has 4 cells, and its layout is a square.
+ * 1 ----- 2
+ * |       |
+ * |       |
+ * |       |
+ * 4 ----- 3
+ */
+int create_small_board(s_board *b)
+{
+	int c;
+
+	init_board(b, SMALL_BOARD_NB_CELLS);
+
+	for (c = 0; c < SMALL_BOARD_NB_CELLS; c++) {
+		b->cells[c].neighbours[0] = &b->cells[(SMALL_BOARD_NB_CELLS + c - 1) % SMALL_BOARD_NB_CELLS];
+		b->cells[c].neighbours[1] = &b->cells[(c + 1) % SMALL_BOARD_NB_CELLS];
+	}
+
+	return 1;
+}
---
+++ src/builder.h
@@ -0,0 +1,5 @@
+#include "entity/board.h"
+
+#define SMALL_BOARD_NB_CELLS 4
+
+int create_small_board(s_board *b);
