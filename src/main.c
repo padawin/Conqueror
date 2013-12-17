fdd080e8f442ede165455db3c27bc25174d0a261
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-12-17 23:45:13 +0100

    for the moment AI are not implemented, only human players are possible

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ int main()
 
 	create_small_board(&b, 2);
 	init_player(&p1, "Player 1", 1, STRATEGY_NONE, NB_PAWNS_PER_PLAYER, NB_CELLS);
-	init_player(&p2, "Player 2", 0, STRATEGY_OFFENCE, NB_PAWNS_PER_PLAYER, NB_CELLS);
+	init_player(&p2, "Player 2", 1, STRATEGY_OFFENCE, NB_PAWNS_PER_PLAYER, NB_CELLS);
 	board_add_player(&b, &p1);
 	board_add_player(&b, &p2);
 
