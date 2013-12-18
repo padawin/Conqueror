19d3c67d28de3f3c98d3ddf2cab7601bec4d5382
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-12-18 13:10:39 +0000

    loop added to prompt a command while no cell is choosen

--- src/entity/player/human.c
+++ src/entity/player/human.c
@@ -18,23 +18,33 @@ short check_command(const char *typed, const char *command, short strict);
  */
 int human_select_cell_to_leave(s_player *p, struct s_cell **player_cells, int nb_cells)
 {
-	int command_size = 30;
+	int command_size, choosen_cell;
+
+	command_size = 30;
 	char command[command_size];
 
-	ui_prompt_command(command, (size_t) command_size);
+	choosen_cell = -1;
 
-	if (check_command(command, "cells", 1)) {
-		ui_list_cells(player_cells, nb_cells);
-	}
-	// list player cells
-	// prompt cell to leave
-	/*
-	 * Command examples:
-	 * cells
-	 * neighbours #
-	 * leave #
-	 */
-	return 1;
+	do {
+		ui_prompt_command(command, (size_t) command_size);
+
+		if (check_command(command, "cells", 1)) {
+			ui_list_cells(player_cells, nb_cells);
+		}
+		else if (check_command(command, "neighbours ", 0)) {
+
+		}
+		// list player cells
+		// prompt cell to leave
+		/*
+		 * Command examples:
+		 * cells
+		 * neighbours #
+		 * leave #
+		 */
+	} while (choosen_cell == -1);
+
+	return choosen_cell;
 }
 
 /**
