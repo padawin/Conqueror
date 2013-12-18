35939b97cf9e9045356fea60c4bc564c2bcc37d0
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-12-18 11:35:17 +0000

    if the typed command is "cells", display the player cells

--- src/entity/player/human.c
+++ src/entity/player/human.c
@@ -10,10 +10,11 @@ int human_select_cell_to_leave(s_player *p, struct s_cell **player_cells, int nb
 	int command_size = 30;
 	char command[command_size];
 
-	ui_list_cells(player_cells, nb_cells);
-
 	ui_prompt_command(command, (size_t) command_size);
 
+	if (check_command(command, "cells")) {
+		ui_list_cells(player_cells, nb_cells);
+	}
 	// list player cells
 	// prompt cell to leave
 	/*
