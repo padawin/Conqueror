70a62a0929be5265742ea7cc8dbe5a0d699f802b
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-12-21 18:39:18 +0000

    test example added

--- tests/main.c
+++ tests/main.c
@@ -1,6 +1,17 @@
 #include "test.h"
+#include "testbuilder.h"
+
+void testbuilder()
+{
+	int a = 1;
+	int b = 2;
+	int c = 2;
+	assert_int_equals(a, b);
+	assert_int_equals(b, c);
+}
 
 int main()
 {
+	testbuilder();
 	return 1;
 }
