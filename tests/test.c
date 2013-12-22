76d7435decfdb48791bc301614d2f14632233d3c
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-12-22 20:44:37 +0000

    assert_not added

--- tests/test.c
+++ tests/test.c
@@ -33,3 +33,37 @@ void assert_null(void *val)
 	else
 		printf("\033[31mKO: The given value is not null\033[00m\n");
 }
+
+
+
+void assert_str_not_equals(char *val1, char *val2)
+{
+	if (strncmp(val1, val2, strlen(val1)) != 0 || strncmp(val2, val1, strlen(val2)) != 0)
+		printf("\033[00;32mOK: Asserting that %s equals %s\033[00m\n", val1, val2);
+	else
+		printf("\033[31mKO: Asserting that %s equals %s\033[00m\n", val1, val2);
+}
+
+void assert_int_not_equals(int val1, int val2)
+{
+	if (val1 != val2)
+		printf("\033[00;32mOK: Asserting that %d equals %d\033[00m\n", val1, val2);
+	else
+		printf("\033[31mKO: Asserting that %d equals %d\033[00m\n", val1, val2);
+}
+
+void assert_long_not_equals(long val1, long val2)
+{
+	if (val1 != val2)
+		printf("\033[00;32mOK: Asserting that %ld equals %ld\033[00m\n", val1, val2);
+	else
+		printf("\033[31mKO: Asserting that %ld equals %ld\033[00m\n", val1, val2);
+}
+
+void assert_not_null(void *val)
+{
+	if (val != NULL)
+		printf("\033[00;32mOK: The given value is null\033[00m\n");
+	else
+		printf("\033[31mKO: The given value is not null\033[00m\n");
+}
