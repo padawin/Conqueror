d413e8cf992fb09776ade8e8ab61fd2a719cb2ab
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-12-22 20:14:27 +0000

    proper tests added for the builder

--- tests/testbuilder.c
+++ tests/testbuilder.c
@@ -7,9 +7,15 @@
 void test_board_creation()
 {
 	s_board b;
+	int c;
 
 	builder_create_small_board(&b, 2);
-	assert_int_equals(b.nb_cells, SMALL_BOARD_NB_CELLS);
-	assert_int_equals(b.nb_players, 0);
-	assert_int_equals(b.nb_total_players, 2);
+
+	for (c = 0; c < SMALL_BOARD_NB_CELLS; c++) {
+		assert_int_equals(b.cells[c].nb_neighbours, 2);
+		assert_int_equals(b.cells[c].neighbours[0]->id, b.cells[(SMALL_BOARD_NB_CELLS + c - 1) % SMALL_BOARD_NB_CELLS].id);
+		assert_int_equals(b.cells[c].neighbours[1]->id, b.cells[(c + 1) % SMALL_BOARD_NB_CELLS].id);
+	}
+
+	free_board(&b);
 }
