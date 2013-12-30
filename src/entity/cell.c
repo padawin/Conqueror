666732e68fc30574f0de8ef8701392ca82bb67a2
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-12-30 10:49:24 +0000

    cell comments added

--- src/entity/cell.c
+++ src/entity/cell.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 #include "cell.h"
 
+/**
+ * Function to initialise a cell.
+ *
+ * @param s_cell *c The cell to initialise
+ * @param const int id The cell ID
+ * @param const int nb_cells The board's number of cells. Used to devine the
+ * 		cell's max number of neigbours.
+ *
+ * @return 1
+ */
 int init_cell(s_cell *c, const int id, const uint8_t nb_cells)
 {
 	c->id = id;
@@ -14,21 +24,54 @@ int init_cell(s_cell *c, const int id, const uint8_t nb_cells)
 	return 1;
 }
 
+/**
+ * Function to free the memory used by a cell. Will free the cell's neighbours,
+ * which have been allocated with a calloc.
+ *
+ * @param s_cell *c The cell to free
+ *
+ * @return void
+ */
 void free_cell(s_cell *c)
 {
 	free(c->neighbours);
 }
 
+/**
+ * Set the cell owner
+ *
+ * @param s_cell *c
+ * @param struct s_player *owner
+ *
+ * @return void
+ */
 void cell_set_owner(s_cell *c, struct s_player *owner)
 {
 	c->owner = owner;
 }
 
+/**
+ * Set the cell's number of pawns
+ *
+ * @param s_cell *c
+ * @param const int nb_pawns
+ *
+ * @return void
+ */
 void cell_set_nb_pawns(s_cell *c, const uint16_t nb_pawns)
 {
 	c->nb_pawns = nb_pawns;
 }
 
+/**
+ * Add a neighbour to a cell.
+ *
+ * @param s_cell *c
+ * @param struct s_cell *neighbour
+ *
+ * @return int 1 if the neighbour is added, ERROR_MAX_NEIGHBOURS_REACHED if the
+ * 		cell cannot have more neighbours.
+ */
 int cell_add_neighbour(s_cell *c, struct s_cell *neighbour)
 {
 	if (c->nb_neighbours == c->nb_max_neighbours)
