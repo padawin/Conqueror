9a285645f9ecb6c4755b4da7f1a939c4b04381ef
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-01-02 17:04:48 +0000

    messages changed

--- tests/test.c
+++ tests/test.c
@@ -39,25 +39,25 @@ void assert_null(void *val)
 void assert_str_not_equals(char *val1, char *val2)
 {
 	if (strncmp(val1, val2, strlen(val1)) != 0 || strncmp(val2, val1, strlen(val2)) != 0)
-		printf("\033[00;32mOK: Asserting that %s equals %s\033[00m\n", val1, val2);
+		printf("\033[00;32mOK: Asserting that %s is different than %s\033[00m\n", val1, val2);
 	else
-		printf("\033[31mKO: Asserting that %s equals %s\033[00m\n", val1, val2);
+		printf("\033[31mKO: Asserting that %s is different than %s\033[00m\n", val1, val2);
 }
 
 void assert_int_not_equals(int val1, int val2)
 {
 	if (val1 != val2)
-		printf("\033[00;32mOK: Asserting that %d equals %d\033[00m\n", val1, val2);
+		printf("\033[00;32mOK: Asserting that %d is different than %d\033[00m\n", val1, val2);
 	else
-		printf("\033[31mKO: Asserting that %d equals %d\033[00m\n", val1, val2);
+		printf("\033[31mKO: Asserting that %d is different than %d\033[00m\n", val1, val2);
 }
 
 void assert_long_not_equals(long val1, long val2)
 {
 	if (val1 != val2)
-		printf("\033[00;32mOK: Asserting that %ld equals %ld\033[00m\n", val1, val2);
+		printf("\033[00;32mOK: Asserting that %ld is different than %ld\033[00m\n", val1, val2);
 	else
-		printf("\033[31mKO: Asserting that %ld equals %ld\033[00m\n", val1, val2);
+		printf("\033[31mKO: Asserting that %ld is different than %ld\033[00m\n", val1, val2);
 }
 
 void assert_not_null(void *val)
