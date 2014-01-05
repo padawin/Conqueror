778d7674d0ac55908171396405892a828bb14f6e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-01-05 17:56:00 +0000

    move and fight properly implemented in player.h

--- src/entity/player.h
+++ src/entity/player.h
@@ -14,6 +14,13 @@ struct s_player;
 #define ERROR_INIT_PLAYER_STRATEGY -2
 #define ERROR_INIT_PLAYER_NB_PAWNS -3
 
+#define PLAYER_ERROR_MOVE_CELLS_NOT_NEIGHBOURS -1
+
+#define NO_FIGHT 0
+#define FIGHT_WON 1
+#define FIGHT_LOST 2
+#define FIGHT_DRAW 3
+
 typedef struct s_player {
 	const char *name;
 	int id;
@@ -33,7 +40,6 @@ int init_player(
 int player_select_cell_to_leave(s_player *p, struct s_cell **player_cells, int nb_cells);
 struct s_cell *player_select_cell_to_go_to(s_player *p, struct s_cell *src_cell);
 uint16_t player_select_nb_pawns(s_player *p, struct s_cell *src_cell);
-int player_move_to_cell(s_player *p, int nb_pawns, struct s_cell *src_cell, struct s_cell *dst_cell);
-int player_fight(s_player *p, int nb_pawns, struct s_cell *cell);
+short player_move_to_cell(s_player *p, uint16_t nb_pawns, struct s_cell *src_cell, struct s_cell *dst_cell);
 
 #endif
